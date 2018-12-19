d37469063fe18461003e5cf18f2822f56405bb1e
adeptio <38818888+adeptio-project@users.noreply.github.com>
2018-12-19 20:09:44 +0200

    Make masternode list full IP

--- masternode_data_to_json.sh
+++ masternode_data_to_json.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 /usr/bin/adeptio-cli masternode list full > /home/explorer/explorer/public/jsondata/masternode_full_list.json
+/usr/bin/adeptio-cli masternode list full | awk '{print $7}' > /home/explorer/explorer/public/jsondata/masternode_full_node_list.json
