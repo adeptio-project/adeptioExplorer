21af5ba61741ecde6786d87e50f4ca06f0fea9a4
Mister Nemo <miister.nemo@gmail.com>
2019-01-11 17:18:07 +0200

    Test

--- routes/index.js
+++ routes/index.js
@@ -401,31 +401,26 @@ router.get('/ext/storade_stats', function(req, res) {
 router.post('/ext/storade_stats', function(req, res) {
 
   var ip = req.connection.remoteAddress
+  var error_result = '{}'
 
-  console.log(ip)
+  console.log(req.params)
+  console.log(req.body)
+  console.log(req.body.search)
+  console.log(locale.ex_search_error)
+  console.log(settings.genesis_tx)
 
   lib.check_IP(ip, function(client_ip){
 
-    console.log(client_ip)
-
     if(!client_ip) {
-        res.send('{}');
-        return
+      res.send(error_result);
+      return
     }
 
     dns.lookup('storadestats.adeptio.cc', function(err, result) {
       lib.check_IP(result, function(storade_stats_ip){
 
-        console.log(storade_stats_ip)
-
-        if(!storade_stats_ip) {
-            res.send('{}');
-            return
-        }
-
-        if(client_ip != storade_stats_ip) {
-          res.send('error');
-          res.end('error');
+        if(!storade_stats_ip || client_ip != storade_stats_ip) {
+          res.send(error_result);
           return
         }
 
