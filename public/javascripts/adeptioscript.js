868ed835e51d6f4defbac1b4b85fc5e357b7224c
Mister Nemo <miister.nemo@gmail.com>
2019-01-14 12:43:01 +0200

    change time format

--- public/javascripts/adeptioscript.js
+++ public/javascripts/adeptioscript.js
@@ -28,7 +28,17 @@
         if (mins<10) mins="0"+mins;
         if (hrs<10) hrs="0"+hrs;
         if (days<10) days="0"+days;
-        return days + " days " + hrs + ':' + mins + ':' + secs + '.' + ms; 
+
+        if (days!="00")
+            return days + " days";
+        else if (hrs!="00")
+            return hrs + ' hours';
+        else if (mins!="00")
+            return mins + ' min';
+        else if (secs!="00")
+            return secs + ' sec';
+        else
+            return ms + ' ms';
     }
 
     function formatBytes(a,b){if(0==a)return"0 Bytes";var c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
